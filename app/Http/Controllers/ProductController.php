c1bafef3228d06421357d66950488e155d20219e
unknown <nguyenanhkhoaa5@gmail.com>
2022-05-30 13:59:28 +0700

    category product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
     {
         //danh muc
         $category = Protype::all();
-        
+
         //san pham moi
         $new_product_phone = Product::leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('products.type_id', '=', 1)->limit(6)->orderBy('product_id', 'DESC')->get();
         $new_product_lap = Product::leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('products.type_id', '=', 2)->limit(6)->orderBy('product_id', 'DESC')->get();
@@ -34,12 +34,21 @@ class ProductController extends Controller
         $topselling_phone = Sale::leftJoin('products', 'products.product_id', '=', 'sales.product_id')->leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('sales.sell_number', '>', '100')->where('products.type_id', '=', '1')->limit(3)->orderBy('sale_id', 'DESC')->get();
         $topselling_laptop = Sale::leftJoin('products', 'products.product_id', '=', 'sales.product_id')->leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('sales.sell_number', '>', '100')->where('products.type_id', '=', '2')->limit(3)->orderBy('sale_id', 'DESC')->get();
         $topselling_sw = Sale::leftJoin('products', 'products.product_id', '=', 'sales.product_id')->leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('sales.sell_number', '>', '100')->where('products.type_id', '=', '4')->limit(3)->orderBy('sale_id', 'DESC')->get();
-        return view('index', compact('new_product_phone','new_product_lap','new_product_tablet','new_product_sw','new_product_tainghe', 'category', 'feature_product_phone','feature_product_lap','feature_product_sw','feature_product_tablet','feature_product_tainghe', 'topselling_phone', 'topselling_laptop', 'topselling_sw'));
+        return view('index', compact('new_product_phone', 'new_product_lap', 'new_product_tablet', 'new_product_sw', 'new_product_tainghe', 'category', 'feature_product_phone', 'feature_product_lap', 'feature_product_sw', 'feature_product_tablet', 'feature_product_tainghe', 'topselling_phone', 'topselling_laptop', 'topselling_sw'));
     }
     public function detail_product($id)
     {
         $pro = Product::leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('product_id', '=', $id)->get();
         $product_ralate = Product::leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('product_id', '=', $id)->get();
-        return view('detail', compact('pro','product_ralate'));
+        return view('detail', compact('pro', 'product_ralate'));
+    }
+    public function category_product($type_id)
+    {
+        $category_product = Product::leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->where('products.type_id', '=', $type_id)->paginate(3);
+        if ($key = request()->key) {
+            $category_product = Product::leftJoin('protypes', 'products.type_id', '=', 'protypes.type_id')->orderBy('product_id', 'DESC')
+                ->where('product_name', 'like', '%' . $key . '%', 'or', 'price', 'like', '%' . $key . '%')->paginate(3);
+        }
+        return view('store', compact('category_product'));
     }
 }
